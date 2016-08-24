534acc18ddd1795e44ce155aacea851cdb98e7a7
Jonathan <jganonpro@outlook.com>
2016-08-24 23:01:30 +0200

    Première commit

---
+++ Personnage.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jonathan
+ * Date: 21/08/2016
+ * Time: 21:44
+ */
+class Personnage
+{
+    private $nom="";
+    private $degats=0;
+
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jonathan
+ * Date: 21/08/2016
+ * Time: 21:44
+ */
+trait index
+{
+
+}
