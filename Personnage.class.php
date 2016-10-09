a3b60de3a7f421af3d7042a542f2177a9069f392
Jonathan <jganonpro@outlook.com>
2016-10-09 10:08:04 +0200

    Seconde commit

--- Personnage.class.php
+++ Personnage.class.php
@@ -8,7 +8,120 @@
  */
 class Personnage
 {
+    private $id;
     private $nom="";
     private $degats=0;
 
+    /**
+     * Personnage constructor.
+     */
+    public function __construct(array $donnees)
+    {
+        $this->hydrate($donnees);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Personnage
+     */
+    public function setId($id)
+    {
+        if(is_integer($id))
+        {
+            $this->id = $id;
+            return $this;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     * @return Personnage
+     */
+    public function setNom($nom)
+    {
+        if(is_string($nom))
+        {
+            $this->nom = $nom;
+            return $this;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getDegats()
+    {
+        return $this->degats;
+    }
+
+    /**
+     * @param int $degats
+     * @return Personnage
+     */
+    public function setDegats($degats)
+    {
+        if(0 < $degats && $degats < 100)
+        {
+            $this->degats = $degats;
+            return $this;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * @param Personnage $personnage
+     * @return string
+     */
+    public function frapper(Personnage $personnage)
+    {
+        if($this->getId() <> $personnage->getId())
+        {
+            $personnage->setDegats($personnage->getDegats() + 5);
+        }
+        else
+        {
+            return "Vous ne pouvez pas vous frapper à vous même. Action annulée";
+        }
+    }
+
+    /**
+     * @param array $donnees
+     */
+    public function hydrate(array $donnees)
+    {
+        foreach ($donnees as $cle => $valeur)
+        {
+            $methode = "set".ucfirst($cle);
+            if(method_exists($methode))
+            {
+                $this->$methode($valeur);
+            }
+        }
+    }
 }
--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-
+include_once ("Personnage.class.php");
 /**
  * Created by PhpStorm.
  * User: Jonathan
  * Date: 21/08/2016
  * Time: 21:44
  */
-trait index
-{
-
-}
+$personnage = new Personnage();
